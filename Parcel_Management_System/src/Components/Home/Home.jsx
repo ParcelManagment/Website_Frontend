ce69633dc5e4463cc3e777d46528c4d78fc6b829
Tharaka Rupasinghe <140690160+TDR-VOID@users.noreply.github.com>
2024-08-20 11:52:20 +0530

    Update Home.jsx

--- Parcel_Management_System/src/Components/Home/Home.jsx
+++ Parcel_Management_System/src/Components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import './Home.css';
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Home = () => {
